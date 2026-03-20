1bb2dd36a6cfa640d4f0e39c0d310f5e8c705ffa
chillerpillar11 <nils.bartels@hotmail.de>
2026-03-20 11:51:58 +0100

    Remove specific variant checks from championship name
    Removed specific checks for 'friday night modern' and 'after work modern' in the championship name.

--- generate_calendar.py
+++ generate_calendar.py
@@ -74,12 +74,6 @@ def is_relevant_event(ev):
     if "championship" in name:
         return True
 
-    # DD-spezifische Varianten
-    if "friday night modern" in name:
-        return True
-    if "after work modern" in name:
-        return True
-
     return False
 
 
